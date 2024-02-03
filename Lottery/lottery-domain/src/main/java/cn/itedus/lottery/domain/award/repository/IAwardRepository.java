af7ccebcad0ff68d4343d505dd36c0c118cbea2b
favorzlx <13726433+favorzlx@user.noreply.gitee.com>
2024-02-03 22:28:21 +0800

    简单工厂搭建发奖领域

---
+++ Lottery/lottery-domain/src/main/java/cn/itedus/lottery/domain/award/repository/IAwardRepository.java
@@ -0,0 +1,11 @@
+package cn.itedus.lottery.domain.award.repository;
+/**
+ * @description: 奖品表仓储服务接口
+ * @author：Favor
+ * @date: 2024/2/3
+ */
+public interface IAwardRepository {
+
+    // TODO 对分库分表中的用户中奖纪录操作
+
+}
---
+++ Lottery/lottery-domain/src/main/java/cn/itedus/lottery/domain/award/repository/impl/AwardRepository.java
@@ -0,0 +1,13 @@
+package cn.itedus.lottery.domain.award.repository.impl;
+
+import cn.itedus.lottery.domain.award.repository.IAwardRepository;
+import org.springframework.stereotype.Component;
+/**
+ * @description: 奖品表仓储服务
+ * @author：Favor
+ * @date: 2024/2/3
+ */
+@Component
+public class AwardRepository implements IAwardRepository {
+
+}
